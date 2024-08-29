18cd9308c7245c9394782da083313bd757cae33a
MwebeCoder27 <mwebraymond13@gmail.com>
2024-08-29 17:50:06 +0300

    init setup

--- initializers/goEnvVariables.go
+++ initializers/goEnvVariables.go
@@ -11,7 +11,6 @@ func LoadEnvVariables() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	// port := os.Getenv("PORT")
 
 	// now do something with s3 or whatever
--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/goJwt/initializers"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+func Hello(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello"})
 }
 func main() {
 	fmt.Println("Hello World")
+
+	r := gin.Default()
+	r.GET("/", Hello)
+	r.Run()
 
 }
